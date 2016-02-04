021e6ffb6949c8bcd67c33670270d990c1cc154d
Toomas Tahves <toomastahves@hotmail.com>
2016-02-04 04:49:46 +0200

    minor

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 import { startWebpackDevServer } from './webpack/devServer';
-import mainApp from './express/mainApp';
+
+import express from 'express';
+const app = express();
+app.use(express.static('public'));
 
 const port = process.env.PORT || 3000;
-mainApp.listen(port, () => {
+app.listen(port, () => {
   console.log(`Express mainApp started at ${port}`);
 });
 
 if(process.env.NODE_ENV !== 'production') {
-  startWebpackDevServer();
+  // startWebpackDevServer();
 }
