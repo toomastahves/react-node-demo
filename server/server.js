90bde056241cef44cc43b7f33805412013f3e78b
Toomas Tahves <toomastahves@hotmail.com>
2016-02-04 13:54:04 +0200

    fix

--- server/server.js
+++ server/server.js
@@ -1,14 +1,11 @@
 import { startWebpackDevServer } from './webpack/devServer';
-
-import express from 'express';
-const app = express();
-app.use(express.static('public'));
+import mainApp from './express/mainApp';
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+mainApp.listen(port, () => {
   console.log(`Express mainApp started at ${port}`);
 });
 
 if(process.env.NODE_ENV !== 'production') {
-  // startWebpackDevServer();
+  startWebpackDevServer();
 }
